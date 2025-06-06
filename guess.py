c157405188c14b800588f0b5a0b3a4f5ea47a5a3
Nico-Lux <soyluffy@gmail.com>
2025-06-06 18:29:29 +0800

    a better version

--- guess.py
+++ guess.py
@@ -7,9 +7,10 @@ while True:
 	guess = int(guess)
 	if guess == r:
 		print('你猜對了！')
+		print('已經猜', count, '次了')
 		break
 	elif guess > r:
 		print('太大了')
 	elif guess < r:
 		print('太小了')
-	print('已經猜',count,'次了')
+	print('已經猜', count, '次了')
